a229359948873f33093115701f0c621f490c713c
sanjana awachat <you@example.com>
2024-06-09 07:31:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink,RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProductsComponent } from './products/products.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent,AboutComponent,BlogComponent,ServicesComponent,ProductsComponent,ProjectsComponent,ContactComponent, FooterComponent,RouterLink,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// // import { AboutComponent } from './about/about.component';
+// import { ServicesComponent } from './services/services.component';
+// import { ProjectsComponent } from './projects/projects.component';
+// import { ContactComponent } from './contact/contact.component';
+// import { BlogComponent } from './blog/blog.component';
+// import { ProductsComponent } from './products/products.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   
+];
